e262c6cf4e6946c9c34d10253925dbb34cee41c0
RYDanielRY <ry.daniil.ry@yandex.ru>
2023-04-08 12:35:18 +0200

    Добавьте файлы проекта.

---
+++ Names.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp61
+{
+    interface Names
+    {
+    }
+
+    public class N : Names
+    {
+
+    }
+}
