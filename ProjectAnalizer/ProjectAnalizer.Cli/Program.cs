137564e5143d6e29a513d8390868ad26149ef2cf
DanielRY <97177840+RYDanielRY@users.noreply.github.com>
2023-04-14 13:37:14 +0200

    Three

--- ProjectAnalizer/ProjectAnalizer.Cli/Program.cs
+++ ProjectAnalizer/ProjectAnalizer.Cli/Program.cs
@@ -1,17 +1,6 @@
 ﻿using ProjectAnalizer.Core;
 
 var path = Path.Combine(Environment.CurrentDirectory, "Input");
-DirectoryInfo directoryInfo = new DirectoryInfo(path);
-if (!directoryInfo.Exists)
-{
-    directoryInfo.Create();
-    Console.WriteLine("Директория создана. Скопируйте файлы для анализа");
-    Console.ReadKey(); 
-}
-else
-{
-    Counting counting = new Counting();
-    counting.pathView(path);
-    counting.Watch();
-}
+CreatingDirectory creatingDirectory = new CreatingDirectory();
+creatingDirectory.Create(path); 
 
---
+++ ProjectAnalizer/ProjectAnalizer.Core/CreatingDirectory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ProjectAnalizer.Core
+{
+    public class CreatingDirectory
+    {
+        public void Create(string path)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            if (!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+                Console.WriteLine("Директория создана. Скопируйте проект для анализа");
+                Console.ReadKey(); 
+            }
+            else
+            {
+                Counting counting = new Counting();
+                counting.pathView(path); 
+                counting.Watch(); 
+            }
+        }
+    }
+}
