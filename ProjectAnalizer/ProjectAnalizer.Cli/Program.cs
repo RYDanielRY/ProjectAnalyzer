be2e2e375d710a80d51a1464c04265c050a97e9a
DanielRY <97177840+RYDanielRY@users.noreply.github.com>
2023-04-14 11:11:30 +0200

    Two

---
+++ ProjectAnalizer/ProjectAnalizer.Cli/Program.cs
@@ -0,0 +1,17 @@
+﻿using ProjectAnalizer.Core;
+
+var path = Path.Combine(Environment.CurrentDirectory, "Input");
+DirectoryInfo directoryInfo = new DirectoryInfo(path);
+if (!directoryInfo.Exists)
+{
+    directoryInfo.Create();
+    Console.WriteLine("Директория создана. Скопируйте файлы для анализа");
+    Console.ReadKey(); 
+}
+else
+{
+    Counting counting = new Counting();
+    counting.pathView(path);
+    counting.Watch();
+}
+
---
+++ ProjectAnalizer/ProjectAnalizer.Core/Counting.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ProjectAnalizer.Core
+{
+    public class Counting
+    {
+        public int countClasses = 0;
+        public int countInterfaces = 0;
+        public int countEnums = 0;
+
+        public void Watch()
+        {
+            Console.WriteLine($"Количество классов: {countClasses}");
+            Console.WriteLine($"Количество интерфейсов: {countInterfaces}");
+            Console.WriteLine($"Количество enums: {countEnums}");
+            Console.ReadKey(); 
+        }
+
+        public void pathView(string path)
+        {
+            var allfolders = Directory.GetDirectories(path);
+            var allfile = Directory.GetFiles(path);
+
+
+            foreach (var folder in allfolders)
+            {
+                pathView(folder);
+            }
+
+            foreach (var file in allfile)
+            {
+                if (file.Contains(".cs") && !file.Contains(".csproj"))
+                {
+                    countObject(file);
+                }
+            }
+        }
+
+        public void countObject(string path)
+        {
+            string toSeOne = File.ReadAllText(path);
+            string pattern = @"(private|private protected|protected|internal|protected internal|public|partial|^|\s)\sclass\s";
+            MatchCollection matches = (Regex.Matches(toSeOne, pattern));
+            var thisCountClass = matches.Count;
+            countClasses += thisCountClass;
+
+            string toSeeInterface = File.ReadAllText(path);
+            string patternInterface = @"(public|^|\s)\sinterface\s";
+            MatchCollection matchesInterface = (Regex.Matches(toSeeInterface, patternInterface));
+            var thisCountInterface = matchesInterface.Count;
+            countInterfaces += thisCountInterface;
+
+            string toSeeEnum = File.ReadAllText(path);
+            string patternEnum = @"(public|private|^|\s)\senum\s";
+            MatchCollection matchesEnum = (Regex.Matches(toSeeEnum, patternEnum));
+            var thisCountEnums = matchesEnum.Count;
+            countEnums += thisCountEnums;
+        }
+    }
+}
