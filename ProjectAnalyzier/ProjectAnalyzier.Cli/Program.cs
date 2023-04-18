abfd40a268711091dfcbd206f56691d6521613e9
DanielRY <97177840+RYDanielRY@users.noreply.github.com>
2023-04-18 15:58:29 +0200

    Six

---
+++ ProjectAnalyzier/ProjectAnalyzier.Cli/Program.cs
@@ -0,0 +1,44 @@
+﻿using ProjectAnalyzier.Core;
+using System;
+using System.IO;
+
+namespace ProjectAnalyzier.Cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                var standartPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input");
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(standartPath);
+
+                if (!directoryInfo.Exists)
+                {
+                    directoryInfo.Create();
+                    Console.WriteLine("Директория создана. Скопируйте файлы для анализа");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    var userPath = standartPath;
+                    ValidateString validateString = new ValidateString(userPath);
+                    validateString.OnValidate();
+                }
+                return;
+            }
+            else
+            {
+                switch (args[0])
+                {
+                    case "-dir" when args.Length == 2:
+                        var userPath = args[1];
+                        ValidateString validateString = new ValidateString(userPath);
+                        validateString.OnValidate();
+                        break;
+                }
+            }
+        }
+    }
+}
